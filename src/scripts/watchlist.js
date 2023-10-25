6d520ca05c980fabf498ffcc1d02356bd455d572
Harrison Liang <liangharrison123@gmail.com>
2023-10-24 21:18:29 -0400

    watchlist with scroll feature

--- src/scripts/watchlist.js
+++ src/scripts/watchlist.js
@@ -10,8 +10,19 @@ const stockList = [
     { symbol: 'NVDA', price: 191.45 },
     { symbol: 'ADBE', price: 635.28 },
     { symbol: 'NFLX', price: 609.63 },
-    { symbol: 'PYPL', price: 275.50 }
+    { symbol: 'PYPL', price: 275.50 },
+    { symbol: 'JPM', price: 151.40 },
+    { symbol: 'V', price: 235.61 },
+    { symbol: 'GS', price: 402.89 },
+    { symbol: 'DIS', price: 170.34 },
+    { symbol: 'VZ', price: 54.29 },
+    { symbol: 'KO', price: 56.48 },
+    { symbol: 'NKE', price: 157.14 },
+    { symbol: 'MCD', price: 233.02 },
+    { symbol: 'PFE', price: 44.50 },
+    { symbol: 'MRK', price: 81.98 },
 ];
+
 
 function displayStockList() {
     const watchlistContainer = document.getElementById('watchlist-container');
